d730744479aba88a57e996fa5a1c84c2270b57d5
Raj Kumar4 <raj_kumar4@epam.com>
2025-01-23 17:32:58 +0530

    largest_vow_word

---
+++ Leetcode_dsa/maxvow_word.js
@@ -0,0 +1,29 @@
+
+function find(sent){
+    let distructure=sent.split(' ');
+    console.log(distructure);
+   const myset=new Set();
+   myset.add('e');
+   myset.add('a');
+   myset.add('i');
+   myset.add('o');
+   myset.add('u');
+   console.log(myset);
+   let count=0;
+   let ans='';
+   for(let word of distructure){
+       let temp=0;
+       for(let letter of word){
+           if(myset.has(letter)){
+               temp++;
+           }
+       }
+       if(count<temp){
+           ans=word;
+       }
+   }
+   return ans;
+}
+let inputsent= prompt("enter the sentence you want to travesrse");
+let max_vowel_word =find(inputsent);
+console.log('maximum vowel containing word in sentence' ,max_vowel_word);
